ee5b94904a1c831010b780955d9b5d3e8a96092c
mauro <maurofm90@gmail.com>
2015-09-12 16:52:11 -0300

    banco

--- app/src/main/java/com/example/caelum/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/example/caelum/agenda/ListaAlunosActivity.java
@@ -1,5 +1,6 @@
 package com.example.caelum.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,7 +9,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,17 +23,29 @@ public class ListaAlunosActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
-        List<String> alunos = Arrays.asList("Ze", "Joao");
+        Button b = (Button) findViewById(R.id.lista_aluno_floating_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        AlunoDAO dao = new AlunoDAO(this);
+        List<Aluno> alunos = dao.getLista();
         ListView lv = (ListView) findViewById(R.id.lista);
         lv.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, alunos));
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String nome = (String) parent.getItemAtPosition(position);
-                Log.i("Nome", nome);
+                Toast.makeText(ListaAlunosActivity.this, nome, Toast.LENGTH_SHORT).show();
             }
         });
     }
-
 
 }
